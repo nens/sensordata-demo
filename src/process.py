8ed67e81205a58d17e94121353ea857fec094f9b
Reinout van Rees <reinout@vanrees.org>
2025-04-11 12:28:56 +0200

    Started extracting water levels

--- src/process.py
+++ src/process.py
@@ -1,3 +1,62 @@
+import datetime
+import logging
+
+from pydantic import BaseModel
+
+GROUNDWATER_DEVICE_NAME = "LevelstickSensoren"
+
+logger = logging.getLogger(__name__)
+
+
+class GroundwaterMeasurement(BaseModel):
+    device_id: str
+    timestamp: datetime.datetime
+    pressure: float
+    temperature: float
+
+
+def _is_groundwater_measurement(item: dict) -> bool:
+    """Return whether it is a proper groundwater measurement including all fields
+
+    The goal is to prevent the rest of the code from having to do too many checks
+    whether dictionary fields exist.
+    """
+    for first in ["time"]:
+        if first not in item:
+            logger.debug(f"{first} not found in item")
+            return False
+
+    for first, second in [
+        ["deviceInfo", "deviceProfileName"],
+        ["deviceInfo", "deviceName"],
+        ["object", "grondwaterdruk"],
+        ["object", "grondwatertemperatuur"],
+    ]:
+        if first not in item:
+            logger.debug(f"{first} not found in item")
+            return False
+        if second not in item[first]:
+            logger.debug(f"{first}>{second} not found in item")
+            return False
+
+    if item["deviceInfo"]["deviceProfileName"] != GROUNDWATER_DEVICE_NAME:
+        return False
+    return True
+
+
+def _convert_to_groundwater_measurement(item: dict) -> GroundwaterMeasurement:
+    return GroundwaterMeasurement(
+        device_id=item["deviceInfo"]["deviceName"],
+        timestamp=item["time"],
+        pressure=item["object"]["grondwaterdruk"],
+        temperature=item["object"]["grondwatertemperatuur"],
+    )
+
+
 def extract_groundwater_measurements(data: list) -> list:
     """Return usable groundwater measurements from the incoming json data"""
-    return []
+    return [
+        _convert_to_groundwater_measurement(item)
+        for item in data
+        if _is_groundwater_measurement(item)
+    ]
--- tests/test_process.py
+++ tests/test_process.py
@@ -1,5 +1,22 @@
+import json
+from pathlib import Path
+
+import pytest
+
 from process import extract_groundwater_measurements
 
+test_dir = Path(__file__).parent
 
-def test_extract_groundwater_measurements():
+
+@pytest.fixture
+def groundwater_data() -> list:
+    example_file = test_dir / "groundwaterlevel-example.json"
+    return json.loads(example_file.read_text())
+
+
+def test_extract_groundwater_measurements1():
     assert extract_groundwater_measurements([]) == []
+
+
+def test_extract_groundwater_measurements2(groundwater_data):
+    assert len(extract_groundwater_measurements(groundwater_data)) == 7
