29a5e658bbb76d0e5c3d708cceddfaa94fb0783a
Reinout van Rees <reinout@vanrees.org>
2025-04-15 16:55:51 +0200

    trying new logging settings

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,27 @@
+accesslog = "-"
+print_config = True
+loglevel = "debug"
+
 logconfig_dict = {
-    "root": {"handlers": ["error_console"], "level": "DEBUG"},
-    "loggers": {},
+    "version": 1,
+    "disable_existing_loggers": True,
+    "formatters": {
+        "verbose": {"format": "%(asctime)s %(name)s %(levelname)s\n    %(message)s"},
+        "simple": {"format": "%(levelname)s %(message)s"},
+    },
+    "handlers": {
+        "null": {"level": "DEBUG", "class": "logging.NullHandler"},
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "": {
+            "handlers": ["console"],
+            "propagate": True,
+            "level": "DEBUG",
+        },
+    },
 }
