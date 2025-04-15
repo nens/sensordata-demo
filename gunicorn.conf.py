48f62b12bd51a922dcb08b42e0acffba6898b30a
Reinout van Rees <reinout@vanrees.org>
2025-04-15 17:01:14 +0200

    disabled custom logging for now

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,26 +2,26 @@ accesslog = "-"
 print_config = True
 loglevel = "debug"
 
-logconfig_dict = {
-    "version": 1,
-    "disable_existing_loggers": True,
-    "formatters": {
-        "verbose": {"format": "%(asctime)s %(name)s %(levelname)s\n    %(message)s"},
-        "simple": {"format": "%(levelname)s %(message)s"},
-    },
-    "handlers": {
-        "null": {"level": "DEBUG", "class": "logging.NullHandler"},
-        "console": {
-            "level": "DEBUG",
-            "class": "logging.StreamHandler",
-            "formatter": "verbose",
-        },
-    },
-    "loggers": {
-        "": {
-            "handlers": ["console"],
-            "propagate": True,
-            "level": "DEBUG",
-        },
-    },
-}
+# logconfig_dict = {
+#     "version": 1,
+#     "disable_existing_loggers": True,
+#     "formatters": {
+#         "verbose": {"format": "%(asctime)s %(name)s %(levelname)s\n    %(message)s"},
+#         "simple": {"format": "%(levelname)s %(message)s"},
+#     },
+#     "handlers": {
+#         "null": {"level": "DEBUG", "class": "logging.NullHandler"},
+#         "console": {
+#             "level": "DEBUG",
+#             "class": "logging.StreamHandler",
+#             "formatter": "verbose",
+#         },
+#     },
+#     "loggers": {
+#         "": {
+#             "handlers": ["console"],
+#             "propagate": True,
+#             "level": "DEBUG",
+#         },
+#     },
+# }
