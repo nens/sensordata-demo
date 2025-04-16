1abbe7dd0f4fb960f5cb8b64ebcf7d179351f012
Reinout van Rees <reinout@vanrees.org>
2025-04-16 18:16:52 +0200

    Added a bit of error logging in case an env var isn't defined

--- src/frost.py
+++ src/frost.py
@@ -1,10 +1,16 @@
 import datetime
+import logging
 import os
 
 import frost_sta_client
 import requests
 
 URL = os.environ.get("FROST_URL", "")
+
+logger = logging.getLogger(__name__)
+
+if not URL:
+    logger.error("Environment variable 'FROST_URL' not defined.")
 
 
 def service():
